16907ba43fc1e324a6c04ef7c0b346b387c3fb76
nealith <nealith@nealith.fr>
2014-05-16 12:48:58 +0200

    lol

---
+++ datas/MirzaDatas.java
@@ -0,0 +1,26 @@
+
+
+
+
+class MirzaDatas {
+
+	private String cheminOrigine;
+
+	private BufferedImage imageCourante;
+
+	private Arraylist<BufferedImage> pileDeModification;
+
+	public MirzaDatas(String path){
+
+	}
+
+	public void charger(String path){
+
+	}
+
+	
+
+
+
+
+}
